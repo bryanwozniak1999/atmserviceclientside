cf78b43fe57c027562d64fbba90f9bab2df6bd27
Olenny Polanco <opolanco1@fordham.edu>
2020-04-03 00:47:21 -0400

    Added deposit message to server

---
+++ src/FordhamBank/Controller.java
@@ -0,0 +1,4 @@
+package FordhamBank;
+
+public class Controller {
+}
---
+++ src/FordhamBank/TransactionWindow.java
@@ -0,0 +1,84 @@
+package FordhamBank;
+
+import java.util.Date;
+
+import FordhamBank.Aggregates.BankAccount;
+import FordhamBank.Aggregates.Transaction;
+import FordhamBank.Enums.TransactionType;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class TransactionWindow {
+	
+	private Stage window;
+	
+	public TransactionWindow(BankAccount account) {
+		
+		initialize(account);
+	}
+	
+	public void show() {
+		window.show();
+	}
+	
+	@SuppressWarnings("unchecked")
+	private void initialize(BankAccount account) {
+		window = new Stage(); // second stage that includes transactions
+		window.setTitle("Transactions");
+		
+		// creates the gui components
+		
+		VBox root = new VBox();
+		root.setSpacing(5);
+		root.setPadding(new Insets(10));
+				
+		Label name = new Label(account.GetAccountName());
+		Label balanceL = new Label("Account Balance");
+		Label balanceNum = new Label(Double.toString(account.GetBalance()));
+		
+		root.getChildren().addAll(name, balanceL, balanceNum);
+		
+		
+		TableView<Transaction> table = new TableView<>();
+		table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+		
+		
+		
+		// the following line is used to create an observableArrayList from the account list of transactions
+		ObservableList<Transaction> data = FXCollections.observableArrayList(account.GetTransactions());
+		
+		
+		
+		TableColumn<Transaction, Date> date = new TableColumn<>("Date");
+		TableColumn<Transaction, String> desc = new TableColumn<>("Description");
+		TableColumn<Transaction, Double> amount = new TableColumn<>("Amount");
+		TableColumn<Transaction, Double> balance = new TableColumn<>("Balance");
+		TableColumn<Transaction, TransactionType> type = new TableColumn<>("Type");
+		
+		// binds each column to an attribute of a Transaction object
+		date.setCellValueFactory(new PropertyValueFactory<Transaction, Date>("TransactionDate"));
+		desc.setCellValueFactory(new PropertyValueFactory<Transaction, String>("TransactionName"));
+		amount.setCellValueFactory(new PropertyValueFactory<Transaction, Double>("Amount"));
+		balance.setCellValueFactory(new PropertyValueFactory<Transaction, Double>("Balance"));
+		type.setCellValueFactory(new PropertyValueFactory<Transaction, TransactionType>("TransactionType"));
+		
+		table.getColumns().addAll(date, desc, amount, balance, type);
+		table.setItems(data);
+		
+		
+		root.getChildren().add(table);
+		
+		Scene scene = new Scene(root, 600, 400);
+		window.setScene(scene);
+		
+		
+	}
+}
